caa8c7914ea1e942d74070ba239ead812b74180c
daviloops <91davidlopez@gmail.com>
2023-05-13 15:19:58 -0700

    Updates

--- server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.60515d34.js","_app/immutable/chunks/index.fe361f2c.js","_app/immutable/chunks/singletons.2ae2bb76.js"];
-export const stylesheets = [];
-export const fonts = [];
