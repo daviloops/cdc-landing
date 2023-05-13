c662042936ff56d12167c3ad7f8a96a1098a924d
daviloops <91davidlopez@gmail.com>
2023-05-13 12:45:39 -0700

    Updates

--- server/manifest.js
+++ server/manifest.js
@@ -1,10 +1,10 @@
 export const manifest = {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["favicon.png"]),
+	assets: new Set([".nojekyll","favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.5403b27e.js","app":"_app/immutable/entry/app.8336c138.js","imports":["_app/immutable/entry/start.5403b27e.js","_app/immutable/chunks/index.fe361f2c.js","_app/immutable/chunks/singletons.4363556d.js","_app/immutable/entry/app.8336c138.js","_app/immutable/chunks/index.fe361f2c.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.0a4791f6.js","app":"_app/immutable/entry/app.38bdc7b5.js","imports":["_app/immutable/entry/start.0a4791f6.js","_app/immutable/chunks/index.fe361f2c.js","_app/immutable/chunks/singletons.2ae2bb76.js","_app/immutable/entry/app.38bdc7b5.js","_app/immutable/chunks/index.fe361f2c.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
--- server/nodes/1.js
+++ server/nodes/1.js
@@ -2,6 +2,6 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.dad775e0.js","_app/immutable/chunks/index.fe361f2c.js","_app/immutable/chunks/singletons.4363556d.js"];
+export const imports = ["_app/immutable/nodes/1.60515d34.js","_app/immutable/chunks/index.fe361f2c.js","_app/immutable/chunks/singletons.2ae2bb76.js"];
 export const stylesheets = [];
 export const fonts = [];
