88706b3dd233a2e90aef9396ef053a98d9642f9e
daviloops <91davidlopez@gmail.com>
2023-05-12 21:34:45 -0700

    Add construction page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+  import '../styles/fonts.css';
+</script>
+
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,96 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // This can be false if you're using a fallback (i.e. SPA mode)
+  export const prerender = true;
+
+  const src = 'src/static/images/asii.svg';
+  const srcFooterImg = "src/static/images/bottomBackground.svg";
+  const srcLogoImg = "src/static/images/logoDark.svg";
+  const srcInProgressImg = "src/static/images/roadBarrier.png";
+
+</script>
+
+<div class="container">
+  <div class="logo-container">
+    <img alt="logo" src={srcLogoImg} class="logo-image" />
+  </div>
+  <div class="content-container">
+    <div class="paper">
+      <div class="in-progress-image-container">
+        <img alt="in-progress" src={srcInProgressImg} class="in-progress-image" />
+      </div>
+      <p class="text">Página en construcción...</p>
+      <a href="https://api.whatsapp.com/send?phone=15551234567" class="link">más información</a>
+    </div>
+  </div>
+  <img alt="background" src={srcFooterImg} class="background-image" />
+</div>
+
+<style>
+	.container {
+    min-height: 100vh;
+    min-width: 100vw;
+    display: flex;
+    flex-direction: column;
+	}
+
+  .background-image {
+    width: 100%;
+    height: auto;
+  }
+
+  .logo-container {
+    width: 100%;
+    padding: 1rem;
+    text-align: center;
+  }
+  
+  .logo-image {
+    width: 208px;
+    height: auto;
+  }
+  
+  .content-container {
+    width: 100%;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    flex-grow: 1;
+  }
+
+  .in-progress-image-container {
+    margin-bottom: 0.5rem;
+  }
+
+  .in-progress-image {
+    width: 91px;
+    height: auto;
+  }
+
+  .paper {
+    width: fit-content;
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #FFF;
+    border-radius: 1rem;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+
+  .text {
+    text-align: center;
+    font-size: 20px;
+    font-weight: 600;
+  }
+
+  .link {
+    text-decoration: none;
+    
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+</style>
